119f2b8a8e7543f48ceddfba07b8d85776d4f38d
awpm9so <awpm9so@gmail.com>
2020-03-02 21:05:25 +0300

    add sort, delete, upadate  post requests

--- app.js
+++ app.js
@@ -25,12 +25,28 @@ const app = express();
 app.set('view engine', 'ejs');
 
 //вывод всех данных из бд 
-app.get('/', (req, res)=>{
-    
-    let sql = 'SELECT * FROM message';
+app.get('/:sort', urlencodedParser, (req, res)=>{
+    res.set('Access-Control-Allow-Origin', '*') 
+    var sort= [];
+    switch (req.params.sort) {
+        case 'asc_num': // сортировка по возрастанию id
+          sort = ['id']
+          break;
+        case 'desc_num': //сортировка по убыванию id
+            sort = ['id DESC']
+          break;
+        case 'asc_alp'://сортировка по алфавиту по столбцу value
+            sort = ['value']
+          break;
+        case 'desc_alp': //сортировка по алфавиту(убывание) по столбцу value
+            sort = ['value DESC']
+            break;
+        default:
+          alert( "Сортировка не задана!" );
+      }
+    let sql = 'SELECT * FROM message ORDER BY '+ sort;
     let query = db.query(sql, (err, result) => {
         if (err) throw err;
-     //console.log(result);
         res.render("index", {messages: result});
 
     });
@@ -44,13 +60,40 @@ app.post('/', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400);
     let post = {value: req.body.message};
     let sql = `INSERT INTO message SET ?`;
-    let query = db.query(sql, post, (err, result) => {
-        if (err) throw err;
+
+    let query = db.query(sql, post, (err, results) => {
+        if (err) res.send(JSON.stringify('Ошибка при добавлении записи!'))
         else
-        res.send('Запись "' + req.body.message +  '" добавлена!');
-    });    
+        res.send(JSON.stringify('Запись добавлена!'))
+    }); 
   })
 
+
+app.post('/:id',urlencodedParser, function (req, res){
+    res.set('Access-Control-Allow-Origin', '*') 
+    let sql = `UPDATE message SET value=? WHERE id=?`;
+    
+    let query = db.query(sql, [req.body.edit, req.params.id], (err, results) => {
+        if (err) {
+            res.send(JSON.stringify('Ошибка при редактировании!'))
+        }
+        else
+        res.send(JSON.stringify('Редактирование прошло успешно!'))
+    });
+});
+
+app.post('/delete/:id',urlencodedParser, function (req, res){
+    res.set('Access-Control-Allow-Origin', '*') 
+    let sql = `DELETE FROM message WHERE id=?`;
+    
+    let query = db.query(sql, [req.params.id], (err, results) => {
+        if (err) {
+            res.send(JSON.stringify('Ошибка при удалении!'))
+        }
+        else
+        res.send(JSON.stringify('Удаление прошло успешно!'))
+    });
+});
 
 
 
